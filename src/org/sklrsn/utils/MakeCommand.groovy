930d5d049d198d364652a176cc9b043c1eecd8a3
Kalai <kalaiarasan.saminathan@ssh.com>
2023-01-05 11:45:08 +0200

    .

--- src/org/sklrsn/utils/MakeCommand.groovy
+++ src/org/sklrsn/utils/MakeCommand.groovy
@@ -3,7 +3,7 @@ package org.sklrsn.utils
 class MakeCommand {
 
     static String prepare(String path, String command) {
-        return path?.trim() ? "make ${command}" : "cd ${path} && make ${command}"
+        return path?.trim() ? "cd ${path} && make ${command}" : "make ${command}"
     }
 
 }
