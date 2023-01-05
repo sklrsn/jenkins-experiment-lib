b846255fe8916b821be7e9841ad83c09ddb21e5b
Kalai <kalaiarasan.saminathan@ssh.com>
2023-01-05 18:55:22 +0200

    .

--- src/org/sklrsn/actions/Alert.groovy
+++ src/org/sklrsn/actions/Alert.groovy
@@ -42,27 +42,20 @@ abstract class Report {
                     switch (stage) {
                         case Stage.UNIT_TESTS:
                             sb.append("${Stage.UNIT_TESTS} - ").append(params.buildUrl).append(Artifacts.UNIT).append(delimiter)
-                            if (params.console && params.console.containsKey(Stage.UNIT_TESTS)) {
-                                sb.append("Console:").append(delimiter)
-                                sb.append(params.console.get(Stage.UNIT_TESTS)).append(delimiter)
-                                sb.append('more logs at ').append(params.buildUrl).append('consoleFull').append(delimiter)
-                            }
+                            this.appendConsoleLogs(params, sb, Stage.UNIT_TESTS, delimiter)
                             break
                         case Stage.SMOKE_TESTS:
                             sb.append("${Stage.SMOKE_TESTS} - ").append(params.buildUrl).append(Artifacts.SMOKE).append(delimiter)
-                            if (params.console && params.console.containsKey(Stage.SMOKE_TESTS)) {
-                                sb.append("Console:").append(delimiter)
-                                sb.append(params.console.get(Stage.UNIT_TESTS)).append(delimiter)
-                                sb.append('more logs at ').append(params.buildUrl).append('consoleFull').append(delimiter)
-                            }
+                            this.appendConsoleLogs(params, sb, Stage.SMOKE_TESTS, delimiter)
                             break
                         case Stage.BUILD_BINARIES:
                             sb.append("${Stage.BUILD_BINARIES} - Failed to compile binaries").append(delimiter)
-                            if (params.console && params.console.containsKey(Stage.UNIT_TESTS)) {
-                                sb.append("Console:").append(delimiter)
-                                sb.append(params.console.get(Stage.UNIT_TESTS)).append(delimiter)
-                                sb.append('more logs at ').append(params.buildUrl).append('consoleFull').append(delimiter)
-                            }
+                            this.appendConsoleLogs(params, sb, Stage.BUILD_BINARIES, delimiter)
+                            /* if (params.console && params.console.containsKey(Stage.UNIT_TESTS)) {
+                                 sb.append("Console:").append(delimiter)
+                                 sb.append(params.console.get(Stage.UNIT_TESTS)).append(delimiter)
+                                 sb.append('more logs at ').append(params.buildUrl).append('consoleFull').append(delimiter)
+                             }*/
                             break
                     }
                 }
@@ -93,6 +86,14 @@ abstract class Report {
         }
 
         return sb.toString()
+    }
+
+    private appendConsoleLogs(Map params, StringBuilder sb, String stage, String delimiter) {
+        if (params.console && params.console.containsKey(stage)) {
+            sb.append("Console:").append(delimiter)
+            sb.append(params.console.get(stage).append(delimiter))
+            sb.append('more logs at ').append(params.buildUrl).append('consoleFull').append(delimiter)
+        }
     }
 
 }
