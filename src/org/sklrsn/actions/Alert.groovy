5ac5130d58e552acc2bfe38076b04ed33775d2a3
Kalai <kalaiarasan.saminathan@ssh.com>
2023-01-05 17:38:02 +0200

    .

--- src/org/sklrsn/actions/Alert.groovy
+++ src/org/sklrsn/actions/Alert.groovy
@@ -24,14 +24,17 @@ abstract class Report {
 
     String report(String delimiter, Map params) {
         StringBuilder sb = new StringBuilder()
-
+        // If BlueOcean is unavailable, fallback to classic page
+        String buildUrl = params.displayUrl?.trim() ? params.displayUrl : params.buildUrl
         switch (params.status) {
             case Status.SUCCESS:
                 sb.append("${Status.SUCCESS}:${params.jobname}-[${params.buildnumber}]").append(delimiter)
+                sb.append("Pipeline - ${buildUrl}").append(delimiter)
                 break
 
             case Status.FAILURE:
                 sb.append("${Status.FAILURE}:${params.jobname}-[${params.buildnumber}]").append(delimiter)
+                sb.append("Pipeline - ${buildUrl}").append(delimiter)
 
                 Set<String> stages = params.stages
                 for (stage in stages) {
@@ -52,14 +55,17 @@ abstract class Report {
 
             case Status.ABORTED:
                 sb.append("${Status.ABORTED}:${params.jobname}-[${params.buildnumber}]").append(delimiter)
+                sb.append("Pipeline - ${buildUrl}").append(delimiter)
                 break
 
             case Status.NORMAL:
                 sb.append("${Status.NORMAL}:${params.jobname}-[${params.buildnumber}]").append(delimiter)
+                sb.append("Pipeline - ${buildUrl}").append(delimiter)
                 break
 
             case Status.UNSTABLE:
                 sb.append("${Status.UNSTABLE}:${params.jobname}-[${params.buildnumber}]").append(delimiter)
+                sb.append("Pipeline - ${buildUrl}").append(delimiter)
                 break
         }
 
@@ -72,10 +78,6 @@ abstract class Report {
             sb.append('changelog:').append(delimiter)
             sb.append(params.changes)
         }
-
-        // If BlueOcean is unavailable, show classic page
-        String buildUrl = params.displayUrl?.trim() ? params.displayUrl : params.buildUrl
-        sb.append("Pipeline - ${buildUrl}").append(delimiter)
 
         return sb.toString()
     }
