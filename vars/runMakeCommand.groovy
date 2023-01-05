04678faaa28ad7858b11bd5f4ce77d3019f22417
Kalai <kalaiarasan.saminathan@ssh.com>
2023-01-05 11:56:25 +0200

    .

--- vars/runMakeCommand.groovy
+++ vars/runMakeCommand.groovy
@@ -4,6 +4,8 @@ def call(Map config) {
     if (config.command?.trim()) {
         def command = MakeCommand.prepare(config.path, config.command)
         sh "${command}"
+
+        return
     }
 
     throw new RuntimeException('Incorrect usage of runMakeCommand')
--- vars/runShCommand.groovy
+++ vars/runShCommand.groovy
@@ -4,6 +4,8 @@ def call(Map config) {
     if (config.command?.trim()) {
         def parsedCmd = ShCommand.parse(config.command)
         sh "${parsedCmd}"
+
+        return
     }
 
     throw new RuntimeException('Incorrect usage of runShCommand.')
