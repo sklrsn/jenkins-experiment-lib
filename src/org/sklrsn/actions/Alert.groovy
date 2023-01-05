5bb6cf146cfd913bddbdd83fb60033fbaa071317
Kalai <kalaiarasan.saminathan@ssh.com>
2023-01-05 15:56:59 +0200

    .

--- src/org/sklrsn/actions/Alert.groovy
+++ src/org/sklrsn/actions/Alert.groovy
@@ -28,27 +28,27 @@ abstract class Report {
         StringBuilder sb = new StringBuilder()
         switch (params.status) {
             case Status.SUCCESS:
-                sb.append("${Status.SUCCESS}:${params.jobname} -[${params.buildnumber}]").append(delimiter)
+                sb.append("${Status.SUCCESS}:${params.jobname}-[${params.buildnumber}]").append(delimiter)
                 sb.append("${params.buildurl}")
                 break
 
             case Status.FAILURE:
-                sb.append("${Status.FAILURE}:${params.jobname} -[${params.buildnumber}]").append(delimiter)
+                sb.append("${Status.FAILURE}:${params.jobname}-[${params.buildnumber}]").append(delimiter)
                 sb.append("${params.buildurl}")
                 break
 
             case Status.ABORTED:
-                sb.append("${Status.ABORTED}:${params.jobname} -[${params.buildnumber}]").append(delimiter)
+                sb.append("${Status.ABORTED}:${params.jobname}-[${params.buildnumber}]").append(delimiter)
                 sb.append("${params.buildurl}")
                 break
 
             case Status.NORMAL:
-                sb.append("${Status.NORMAL}:${params.jobname} -[${params.buildnumber}]").append(delimiter)
+                sb.append("${Status.NORMAL}:${params.jobname}-[${params.buildnumber}]").append(delimiter)
                 sb.append("${params.buildurl}")
                 break
 
             case Status.UNSTABLE:
-                sb.append("${Status.UNSTABLE}:${params.jobname} -[${params.buildnumber}]").append(delimiter)
+                sb.append("${Status.UNSTABLE}:${params.jobname}-[${params.buildnumber}]").append(delimiter)
                 sb.append("${params.buildurl}")
                 break
         }
