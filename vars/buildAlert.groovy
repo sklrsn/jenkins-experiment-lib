fed373817aeba6c9113985d44f28ef9537bad191
Kalai <kalaiarasan.saminathan@ssh.com>
2023-01-09 10:08:37 +0200

    .

--- vars/buildAlert.groovy
+++ vars/buildAlert.groovy
@@ -7,6 +7,7 @@ def call(Map config) {
     if (config.medium?.trim() && config.jobname?.trim() && config.status?.trim() && config.buildnumber?.trim() && config.buildUrl?.trim()) {
         def message = Alert.generate(config)
 
+        println(config.console)
         String color = Color.INFO
         if (config.color?.trim()) {
             color = config.color
