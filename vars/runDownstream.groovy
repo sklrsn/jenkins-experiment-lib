c1099df3f2f26c5ef7c102b1d70b787e729bddb7
Kalai <kalaiarasan.saminathan@ssh.com>
2023-01-11 23:58:59 +0200

    .

--- vars/runDownstream.groovy
+++ vars/runDownstream.groovy
@@ -1,6 +1,7 @@
 def call(Map params){
     List jobs = params.jobs
-    for (job in jobs) {
-        build wait:false job:"${job}"
+    println(jobs)
+    for (_job in jobs) {
+        build wait:false job:"${_job}"
     }
 }
